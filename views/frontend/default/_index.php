cf21dca7c4eaa49fdb6a2bd5e2b793f73dfcfd92
sedacelik <46072011+sedacelik@users.noreply.github.com>
2018-12-26 23:34:25 +0300

    Update _index.php

--- views/frontend/default/_index.php
+++ views/frontend/default/_index.php
@@ -2,7 +2,7 @@
 use kouosl\theme\helpers\Html;
 use kouosl\theme\widgets\Portlet;
 
-$this->title = 'Index Sample';
+$this->title = 'seda';
 $data['title'] = Html::encode($this->title);
 $this->params['breadcrumbs'][] = $this->title;
 
